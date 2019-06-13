b48c8cdf4b2606202e613bcb8d1a4455098bb3df
Teiyanana <312248024@qq.com>
2019-06-13 10:22:03 +0800

    construct using browserify

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # ts-learn
 
-使用gulp构建
+把工程从Node.js环境移到浏览器环境。
+
+使用browserify将所有模块打包为一个js文件，browserify支持Node.js的CommonJS模块，而typescript默认生成commonJS类型。
+
+tsify是browserify的一个插件，作用和gulp-typescript一样能够访问typescript编译器。
+
+vinyl-source-stream会将Browserify输出的文件适配gulp能够解析的格式。
+
+建立一个index.html文件来引用打包后的js文件。
 
 ```
 gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
 
-gulp.task('default', function() {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+var path = {
+  pages: ['src/*.html']
+};
+
+gulp.task('copy-html', function() {
+  return gulp.src(path.pages)
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', gulp.series('copy-html', function() {
+  return browserify({
+    basedir: '.',
+    debug: true, // tsify在输出文件里生成source maps
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify) // 使用tsify调用browserify
+    .bundle()
+    .pipe(source('bundle.js')) // 输出文件名bundle.js
+    .pipe(gulp.dest('dist'));
+}));
